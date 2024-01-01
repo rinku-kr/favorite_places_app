b33aa24c489246f47eac1904c867e3b660697beb
rinku-kr <rk24736599@gmail.com>
2024-01-02 00:44:13 +0530

    Adding riverpod & a provider

---
+++ lib/provider/user_places.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_places/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider((ref) => UserPlacesNotifier());
